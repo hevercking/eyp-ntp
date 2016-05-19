1f89c74135b6e6f6b2540a018b0ac7290b8106d8
Jordi Prats <jprats@systemadmin.es>
2016-05-19 17:54:15 +0200

    afegit testing config file

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -30,6 +30,20 @@ describe 'ntp class' do
         it { should be_enabled }
         it { is_expected.to be_running }
       end
+
+      describe file('/etc/ntp.conf') do
+        it { should be_file }
+        its(:content) { should match 'puppet managed file' }
+        its(:content) { should match 'server 1.ie.pool.ntp.org iburst' }
+        its(:content) { should match 'server 0.europe.pool.ntp.org iburst' }
+        its(:content) { should match 'server 3.europe.pool.ntp.org iburst' }
+        its(:content) { should match 'driftfile /etc/ntp.drift' }
+        its(:content) { should match 'restrict -4 default kod nomodify notrap nopeer noquery' }
+        its(:content) { should match 'restrict -6 default kod nomodify notrap nopeer noquery' }
+        its(:content) { should match 'restrict 127.0.0.1' }
+        its(:content) { should match 'restrict ::1' }
+      end
+
     end
 
   end
