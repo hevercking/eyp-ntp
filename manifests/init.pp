19570d67511c18b5f2254f14735e3ee7515a9567
Jordi Prats <jprats@systemadmin.es>
2016-05-19 16:19:32 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,17 +3,17 @@
 # === ntp documentation
 #
 class ntp (
-            $manage_package = true,
-            $package_ensure = 'installed',
-            $driftfile      = $ntp::params::driftfile_default,
-            $tinker         = $tinker_default,
-            $tinker_panic   = $tinker_panic_default,
-            $servers        = $servers_default,
-            $iburst         = true,
-            $manage_service = true,
+            $manage_package        = true,
+            $package_ensure        = 'installed',
+            $driftfile             = $ntp::params::driftfile_default,
+            $tinker                = $tinker_default,
+            $tinker_panic          = $tinker_panic_default,
+            $servers               = $servers_default,
+            $iburst                = true,
+            $manage_service        = true,
             $manage_docker_service = true,
-            $service_ensure = 'running',
-            $service_enable = true,
+            $service_ensure        = 'running',
+            $service_enable        = true,
           ) inherits ntp::params {
 
   validate_array($servers)
