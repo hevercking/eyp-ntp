59bc292f1a8e19e3c71cddb91e4be870a788230e
Jordi Prats <jprats@systemadmin.es>
2016-07-06 13:42:23 +0200

    bugfix is_expected

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,9 +7,10 @@ class ntp::service() inherits ntp {
 
   validate_re($ntp::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${ntp::service_ensure}")
 
-  if(getvar('::eyp_docker_iscontainer')==undef or
-      getvar('::eyp_docker_iscontainer')==false or
-      getvar('::eyp_docker_iscontainer') =~ /false/ or
+  $is_docker_container_var=getvar('::eyp_docker_iscontainer')
+  $is_docker_container=str2bool($is_docker_container_var)
+
+  if( $is_docker_container==false or
       $ntp::manage_docker_service)
   {
     if($ntp::manage_service)
