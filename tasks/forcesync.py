6dd8e9ce547aef6f9b2b0000cf050a305cc3a8bf
Jordi Prats <jordi.prats@gmail.com>
2019-03-28 13:27:44 +0100

    otes

--- tasks/forcesync.py
+++ tasks/forcesync.py
@@ -9,9 +9,13 @@ params = json.load(sys.stdin)
 
 ntpserver = params['ntpserver']
 
+# puppet resource service ntp ensure=stopped
+
 result={}
 result['forcesync']={}
 result['forcesync']['server']=ntpserver
 result['forcesync']['output']=subprocess.check_output("ntpdate "+ntpserver, shell=True)
+
+# puppet resource service ntp ensure=running
 
 print(json.dumps(result))
